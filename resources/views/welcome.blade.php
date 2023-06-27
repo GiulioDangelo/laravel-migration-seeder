ab482468afb51b70034fb9f0f4177f1c12707994
GiulioDangelo <dangelogiulio48@gmail.com>
2023-06-27 19:47:45 +0200

    display db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,38 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <h1 class="text-center">Ciao da vite</h1>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
+        <div class="container">
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th class="text-center text-danger">Company</th>
+                        <th class="text-center text-danger">Departure Station</th>
+                        <th class="text-center text-danger">Arrival Station</th>
+                        <th class="text-center text-danger">Departure Date</th>
+                        <th class="text-center text-danger">Arrival Date</th>
+                        <th class="text-center text-danger">Train Code</th>
+                        <th class="text-center text-danger">Number Of Carriages</th>
+                        <th class="text-center text-danger">In time</th>
+                        <th class="text-center text-danger">Deleted</th>
+        
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($trains as $item)
+                        <tr>
+                            <td class="text-center">{{ $item->azienda }}</td>
+                            <td class="text-center">{{ $item->stazione_partenza}}</td>
+                            <td class="text-center">{{ $item->stazione_arrivo }}</td>
+                            <td class="text-center">{{ substr($item->ora_partenza , 0, -3) }}</td>
+                            <td class="text-center">{{ substr($item->ora_arrivo ,0, -3) }}</td>
+                            <td class="text-center">{{ $item->codice_treno }}</td>
+                            <td class="text-center">{{ $item->carrozze }}</td>
+                            <td class="text-center">{{ $item->in_orario ? 'si' :  'no' }}</td>
+                            <td class="text-center">{{ $item->cancellato ? 'si' :  'no'}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
